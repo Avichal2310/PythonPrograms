d954ce24d091e5e98b29b2fa40a1b0b93594235b
Avichal2310 <119150433+Avichal2310@users.noreply.github.com>
2023-03-18 19:29:32 +0530

    Create Program3.py

---
+++ Program3.py
@@ -0,0 +1,11 @@
+'''
+Input height and length of a right angled triangle and show its area.
+
+Hint=use formula 1/2*h*l
+'''
+print("Enter height of a right angled triangle")
+h=int(input())
+print("Enter length of a right angled triangle")
+l=int(input())
+result=1/2*h*l
+print("Area of right angled triangle is",result)
