8bd106b87bbc557c065155a16df827952e1f1e6a
Avichal2310 <119150433+Avichal2310@users.noreply.github.com>
2023-03-18 20:07:39 +0530

    Create Program8.py

---
+++ Program8.py
@@ -0,0 +1,9 @@
+'''
+Write a program to input a number and check it to be even or odd.
+'''
+print("Enter a number")
+n=int(input())
+if n%2==0:
+    print(n,"is even")
+else:
+    print(n,"is odd")
