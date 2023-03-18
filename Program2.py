bd736eda6a4e908801fc42de16b31687ace62ade
Avichal2310 <119150433+Avichal2310@users.noreply.github.com>
2023-03-18 19:27:11 +0530

    Create Program2.py

---
+++ Program2.py
@@ -0,0 +1,10 @@
+'''
+If radius of a circle is 2.3cm then calculate it's circumference and area.
+Hint=use formula 2*pi*r and pi*r*r 
+'''
+import math 
+r=2.3
+circumference=2*math.pi*r
+print("Circumference is",circumference)
+area=math.pi*r*r
+print("Area is",area)
