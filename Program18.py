d2e8cbc486f605c78aec0c63dfd9969035d2902e
Avichal2310 <119150433+Avichal2310@users.noreply.github.com>
2023-04-20 22:03:14 +0530

    Create Program18.py

---
+++ Program18.py
@@ -0,0 +1,16 @@
+'''
+Write a program to input a number and print into words.
+'''
+print("Enter a number")
+n=input()
+for d in n:
+    if d=="0": print("Zero",end=" ")
+    elif d=="1": print("One",end=" ")
+    elif d=="2": print("Two",end=" ")
+    elif d=="3": print("Three",end=" ")
+    elif d=="4": print("Four",end=" ")
+    elif d=="5": print("Five",end=" ")
+    elif d=="6": print("Six",end=" ")
+    elif d=="7": print("Seven",end=" ")
+    elif d=="8": print("Eight",end=" ")    
+    elif d=="9": print("Nine",end=" ")
