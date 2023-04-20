ee2af19d4fda23273ffb27554042cbb1067ddac7
Avichal2310 <119150433+Avichal2310@users.noreply.github.com>
2023-04-20 22:08:51 +0530

    Create Program24.py

---
+++ Program24.py
@@ -0,0 +1,116 @@
+'''
+Write a program to input two colours and enter their mixture.
+'''
+print("Enter two colours for mixture")
+c=input()
+o=input()
+if c=='red' and o=='black':
+    print("Mixture of these colours will be dark red colour")
+elif c=='red' and o=='grey':
+    print("Mixture of these colours will be dull red colour")
+elif c=='red' and o=='white':
+    print("Mixture of these colours will be pink colour")
+elif c=='red' and o=='pink':
+    print("Mixture of these colours will be dark pink colour")
+elif c=='red' and o=='orange':
+    print("Mixture of these colours will be vermilion colour")
+elif c=='red' and o=='yellow':
+    print("Mixture of these colours will be orange colour")
+elif c=='red' and o=='green':
+    print("Mixture of these colours will be yellow colour")
+elif  c=='red' and o=='blue': 
+    print("Mixture of these colours will be purple colour") 
+elif c=='red' and o=='purple':
+    print("Mixture of these colours will be magenta colour") 
+elif c=='red' and o=='brown':                                 
+    print("Mixture of these colours will be maroon colour")
+elif c=='black' and o=='grey':
+    print("Mixture of these colours will be darker grey colour")
+elif c=='black' and o=='white':
+    print("Mixture of these colours will be gray colour")
+elif c=='black' and o=='pink':
+    print("Mixture of these colours will be dark pink colour")
+elif c=='black' and o=='orange':
+    print("Mixture of these colours will be orange colour")
+elif c=='black' and o=='yellow':
+    print("Mixture of these colours will be green colour")
+elif c=='black' and o=='green':
+    print("Mixture of these colours will be dark green colour")
+elif c=='black' and o=='blue':
+    print("Mixture of these colours will be dark blue colour")
+elif c=='black' and o=='purple':
+    print("Mixture of these colours will be dark purple colour")
+elif c=='black' and o=='brown':
+    print("Mixture of these colours will be blacky brown colour")
+elif c=='grey' and o=='white':
+    print("Mixture of these colours will be haze grey colour")
+elif c=='grey' and o=='pink':
+    print("Mixture of these colours will be lavender colour")
+elif c=='grey' and o=='orange':
+    print("Mixture of these colours will be brown colour")
+elif c=='grey' and o=='yellow':
+    print("Mixture of these colours will be olive green colour")
+elif c=='grey' and o=='green':
+    print("Mixture of these colours will be dull green colour")
+elif c=='grey' and o=='blue':
+    print("Mixture of these colours will be blue grey colour")
+elif c=='grey' and o=='purple':
+    print("Mixture of these colours will be greyish purple colour")
+elif c=='grey' and o=='brown':
+    print("Mixture of these colours will be taupe colour")
+elif c=='white' and o=='pink':
+    print("Mixture of these colours will be light pink colour")
+elif c=='white' and o=='orange':
+    print("Mixture of these colours will be light orange colour")
+elif c=='white' and o=='yellow':
+    print("Mixture of these colours will be light yellow colour")
+elif c=='white' and o=='green':
+    print("Mixture of these colours will be light green colour")
+elif c=='white' and o=='blue':
+    print("Mixture of these colours will be light blue colour")
+elif c=='white' and o=='purple':
+    print("Mixture of these colours will be light purple colour")
+elif c=='white' and o=='brown':
+    print("Mixture of these colours will be tan colour")
+elif c=='pink' and o=='orange':
+    print("Mixture of these colours will be peach colour")
+elif c=='pink' and o=='yellow':
+    print("Mixture of these colours will be peachy orange colour")
+elif c=='pink' and o=='green':
+    print("Mixture of these colours will be brown shaded colour")
+elif c=='pink' and o=='blue':
+    print("Mixture of these colours will be purple shaded colour")
+elif c=='pink' and o=='purple':
+    print("Mixture of these colours will be magenta colour")
+elif c=='pink' and o=='brown':
+    print("Mixture of these colours will be garnet colour")
+elif c=='orange' and o=='yellow':
+    print("Mixture of these colours will be amber colour")
+elif c=='orange' and o=='green':
+    print("Mixture of these colours will be light brown colour")
+elif c=='orange' and o=='blue':
+    print("Mixture of these colours will be brown colour")
+elif c=='orange' and o=='purple':
+     print("Mixture of these colours will be reddish brown colour")
+elif c=='orange' and o=='brown':
+    print("Mixture of these colours will be terracotta colour")
+elif c=='yellow' and o=='green':
+    print("Mixture of these colours will be yellow-green colour")
+elif c=='yellow' and o=='blue':
+    print("Mixture of these colours will be green colour")
+elif c=='yellow' and o=='purple':
+    print("Mixture of these colours will be brown colour")
+elif c=='yellow' and o=='brown':
+    print("Mixture of these colours will be lighter brown colour")
+elif c=='green' and o=='blue':
+    print("Mixture of these colours will be cyan colour")
+elif c=='green' and o=='purple':
+    print("Mixture of these colours will be brown colour")
+elif c=='green' and o=='brown':
+    print("Mixture of these colours will be darker green colour")
+elif c=='blue' and o=='purple':
+    print("Mixture of these colours will be violet colour")
+elif c=='blue' and o=='brown':
+    print("Mixture of these colours will be dark blue colour")
+elif c=='purple' and o=='brown':
+    print("Mixture of these colours will be plum colour")
